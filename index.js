12620fc126e10f3d240df031891fc5f1817e3915
blanco92i <blancotradizi@gmail.com>
2024-11-09 22:02:13 +0000

    request headers

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req,res)=>{
+  let ipadress = req.ip;
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+  res.json({
+    ipadress:ipadress,
+    language:language,
+    software:software
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
